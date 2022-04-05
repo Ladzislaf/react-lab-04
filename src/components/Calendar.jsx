768d0ecf7a89037510742d5afd3784c511584a6f
ladzislaf <ladzislaf@gmail.com>
2022-04-05 20:51:01 +0300

    refactor

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,59 +1,58 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import CalendarBody from './CalendarBody'
 import CalendarHead from './CalendarHead'
+
 import './style/Calendar.css'
 
-const Calendar = () => {
+const CalendarOld = () => {
     const [currentMonth, setCurrentMonth] = useState(new Date().getMonth())
     const [currentYear, setCurrentYear] = useState(new Date().getFullYear())
-    const [pickedDays, setPickedDays] = useState([])
+    const [pickedDaysList, setPickedDaysList] = useState([])
 
     const handlePrevClick = () => {
-        setCurrentMonth(currentMonth - 1)
+        (currentMonth === 0) ? setCurrentMonth(11) : setCurrentMonth(currentMonth - 1)
         if (currentMonth % 12 === 0) {
             setCurrentYear(currentYear - 1)
         }
     }
 
     const handleNextClick = () => {
-        setCurrentMonth(currentMonth + 1)
-        if ((currentMonth + 1200) % 12 === 11) {
+        setCurrentMonth((currentMonth + 1) % 12)
+        if (currentMonth % 12 === 11) {
             setCurrentYear(currentYear + 1)
         }
     }
 
-    const handleDayClick = (e, month, year) => {
-		let day = new Date(year, month, e.target.dataset.user)
-
-		for (let i = 0; i < pickedDays.length; i++) {
-			if (pickedDays[i].valueOf() === day.valueOf()) {
-				let copy = pickedDays.slice()
-				copy.splice(i, 1)
-				// remove day
-				setPickedDays(copy)
-				return
-			}
-		}
-		// add new day
-		setPickedDays([...pickedDays, day])
+    const handleDayClick = (pickDay) => {
+        for (let i = 0; i < pickedDaysList.length; i++) {
+            if (pickedDaysList[i].date.toString() === pickDay.date.toString()) {
+                let copy = pickedDaysList.slice()
+                copy.splice(i, 1)
+                setPickedDaysList(copy)
+                return
+            }
+        }
+        setPickedDaysList([...pickedDaysList, pickDay])
     }
 
     return (
-        <div className={'cal'}>
-            <CalendarHead
-                currentMonth={currentMonth}
-                currentYear={currentYear}
-                handlePrevClick={handlePrevClick}
-                handleNextClick={handleNextClick}
-            />
-            <CalendarBody
-                currentMonthIndex={currentMonth}
-                handleDayClick={handleDayClick}
-				pickedDays={pickedDays}
-            />
-            <div>{pickedDays.value}</div>
-        </div>
+        <>
+            <div className={'cal'}>
+                <CalendarHead
+                    currentMonth={currentMonth}
+                    currentYear={currentYear}
+                    handlePrevClick={handlePrevClick}
+                    handleNextClick={handleNextClick}
+                />
+                <CalendarBody
+                    currentMonthIndex={currentMonth}
+                    currentYear={currentYear}
+                    handleDayClick={handleDayClick}
+                    pickedDaysList={pickedDaysList}
+                />
+            </div>
+        </>
     )
 }
 
-export default Calendar
+export default CalendarOld
